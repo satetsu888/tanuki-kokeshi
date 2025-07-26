aed8078a1b28a3b0e0dacd35c3dc05fe615cd5c6
satetsu888 <satetsu888@gmail.com>
2025-07-26 14:28:59 +0900

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/tanuki-kokeshi/',
   root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   },
   server: {
     port: 3000,
